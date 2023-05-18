2551e61420f8b5cabdc7db5dc276d673e5daf3d3
mehekj <mehek.jethani@gmail.com>
2023-05-18 12:44:58 -0400

    testing update auto deploy

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <div className="space-y-4">
           <h1 className="text-center text-5xl">NextJS 13 Blog</h1>
           <p className="text-center text-xl">
-            Welcome to a dynamic markdown blog using NextJS 13.
+            Testing update 123
           </p>
         </div>
 
