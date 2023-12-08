0512dcc40417514bdfb787f03a23badad486af60
mehekj <mehek.jethani@gmail.com>
2023-12-08 12:13:40 -0500

    better

--- app/memories/page.tsx
+++ app/memories/page.tsx
@@ -8,15 +8,4 @@ export default function Memories() {
             'https://youtu.be/9EcjWd-O4jI?si=0J0UfeFXVahAqwo9'
         );
     });
-
-    return (
-        <div>
-            <a
-                className="link"
-                href="https://youtu.be/9EcjWd-O4jI?si=0J0UfeFXVahAqwo9"
-            >
-                redirect
-            </a>
-        </div>
-    );
 }
